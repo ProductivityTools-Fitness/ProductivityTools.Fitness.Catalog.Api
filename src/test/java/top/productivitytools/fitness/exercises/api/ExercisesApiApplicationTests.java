f021056b9e6bffc8a14fe42fa2b3b30053411d48
Pawel Wujczyk <pwujczyk@gmail.com>
2026-09-17 19:17:35 +0000

    github

--- src/test/java/top/productivitytools/fitness/exercises/api/ExercisesApiApplicationTests.java
+++ src/test/java/top/productivitytools/fitness/exercises/api/ExercisesApiApplicationTests.java
@@ -1,13 +1,31 @@
 package top.productivitytools.fitness.exercises.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ExercisesApiApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void debugHelloReturnsHello() throws Exception {
+		mockMvc.perform(get("/api/debug/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello"));
 	}
 
 }
